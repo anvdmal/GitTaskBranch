0a7fba64ce960bc555ebc309fd4713843deb53b9
anvdmal <alekseeva.ann04@mail.ru>
2024-11-15 17:45:41 +0300

    add GoodEvening.java in branch dev

---
+++ src/src/main/java/gitLabs/GoodEvening.java
@@ -0,0 +1,9 @@
+package gitLabs;
+
+import java.time.LocalDate;
+
+public class GoodEvening {
+    public static void main(String[] args) {
+        System.out.println("Добрый вечер в ветке dev! Сегодняшняя дата: " + LocalDate.now());
+    }
+}
