f579eb79d70bd740a719cfbb51248132b03c0749
anvdmal <alekseeva.ann04@mail.ru>
2024-11-15 17:32:29 +0300

    add GoodEvening.java

---
+++ src/src/main/java/gitLabs/GoodEvening.java
@@ -0,0 +1,9 @@
+package gitLabs;
+
+import java.time.LocalDate;
+
+public class GoodEvening {
+    public static void main(String[] args) {
+        System.out.println("Добрый вечер! Сегодняшняя дата: " + LocalDate.now());
+    }
+}
